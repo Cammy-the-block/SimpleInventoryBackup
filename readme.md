09bff11009be5b2a73bb2927f336842f9569aa10
Cammy-the-block <awesomepossumcraft@gmail.com>
2015-07-01 12:00:05 -0700

    Added world support

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 #SimpleInventoryBackup
 ###Inventory backups made easy!
-SimpleInventoryBackup allows server administrators to backup specific or all players inventories and restore them as they wish. The source can be seen at the Github page.
+SimpleInventoryBackup allows server administrators to backup specific or all players inventories and restore them as they wish. The source can be seen at the [Github page](https://github.com/Cammy-the-block/SimpleInventoryBackup) Feel free to download the plugin at the [Bukkit page](http://dev.bukkit.org/bukkit-plugins/simpleinventorybackup/).
 
 ##Features
 * Server Admins can backup all players' inventories or a chosen player's inventory in one command
@@ -9,12 +9,17 @@ SimpleInventoryBackup allows server administrators to backup specific or all pla
 
 ##Commands
 **/backupinventory <player>** - Backs up a given player's inventory. To back up all players' inventories use all in place of a player name.
-**/restoreInventory <player>** - Restores a given player's inventory to the backup. To restore all player's inventories use all in place of a player name.
-**/requestInventoryRestore** - Request that an admin restore their inventory.
+**/restoreInventory <player>** - Restores a given player's inventory to the backup. To restore ever players' inventories use all in place of a player name.
+**/requestinventoryrestore** - Request that an admin restore their inventory.
 
 ##Permissions
 **simpleinventorybackup.backup** - Allows a player to backup inventories.
 **simpleinventorybackup.restore** - Allows a player to restore inventories.
 **simpleinventorybackup.request** - Allows a player to request to have their inventories
 **simpleinventorybackup.seerequest** - If this permission is granted, the requests of players to have their inventories backed up are shown to the player with this permission. 
+
+##Configuration
+**individualplayeroverride** - Boolean value. True causes an inventory backup or restore of a specific player to occur regardless of the excluded and included worlds. False causes inventory backups and restores to occur as dictated by the config.
+**includedworlds** - Comma seperated worlds or all. Backups and restores will only occur on included worlds. Obviously all includes all worlds :P If a world is excluded that shall override including all.
+**excludedworlds** - Comma seperated worlds. Worlds listed here will not hav inventory backups or restores occur on them.
 
--- src/io/github/cammy_the_block/SimpleInventoryBackup.java
+++ src/io/github/cammy_the_block/SimpleInventoryBackup.java
@@ -1,14 +1,20 @@
 package io.github.cammy_the_block;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
@@ -16,16 +22,29 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SimpleInventoryBackup  extends JavaPlugin {
+	
+	List<World> worldsToInclude = new ArrayList<World>();
+	List<World> worldsToExclude = new ArrayList<World>();
+	boolean excludeAllWorlds = false;
+	boolean includeAllWorlds = false;
+	boolean noWorlds = false;
+	boolean noExcludedWorlds = true;
+	boolean individualPlayerOverride = false;
+	File f = new File("plugins/SimpleInventoryBackup/playerInventoryBackups.yml");
+	YamlConfiguration yaml = YamlConfiguration.loadConfiguration(f);
 
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(new TheListener(), this);
+		getConfig().addDefault("individualplayeroverride", true);
+		getConfig().addDefault("includedworlds", "all");
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+		readConfig();
 	}
 	@Override
 	public void onDisable() {
 		HandlerList.unregisterAll(new TheListener());
-		getConfig().options().copyDefaults(true);
-		saveConfig();
 	}
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
@@ -54,22 +73,15 @@ public class SimpleInventoryBackup  extends JavaPlugin {
 			return true;
 		}
 	}
+	//
+	//INVENTORY RESTORE METHODS
+	//
 	private boolean restoreInventory(CommandSender sender, Command cmd, String label, String[] args) {
 		if(args.length == 1){
 			if(args[0].equalsIgnoreCase("all")){
-				sender.sendMessage("Restoring all inventories.");
+				sender.sendMessage("Restoring all inventories acording.");
 				for(Player p : Bukkit.getServer().getOnlinePlayers()) {
-					List<ItemStack> items = new ArrayList<ItemStack>();
-					for(int x = 0; x < 36; x++){
-						items.add(getConfig().getItemStack(p.getName() + x));
-					}
-					p.getInventory().setContents(items.toArray(new ItemStack[items.size()]));
-
-					List<ItemStack> armorItems = new ArrayList<ItemStack>();
-					for(int x = 0; x < 4; x++){
-						armorItems.add(getConfig().getItemStack(p.getName() + "a" + x));
-					}
-					p.getInventory().setArmorContents(armorItems.toArray(new ItemStack[armorItems.size()]));	
+					restoreInventoryIfSupposedTo(p);
 				}
 				sender.sendMessage("All inventories have been restored.");
 				return true;
@@ -77,18 +89,13 @@ public class SimpleInventoryBackup  extends JavaPlugin {
 			else {
 				Player p = Bukkit.getPlayer(args[0]);
 				if (p != null) {
-					List<ItemStack> items = new ArrayList<ItemStack>();
-					for(int x = 0; x < 36; x++){
-						items.add(getConfig().getItemStack(p.getName() + x));
+					if(individualPlayerOverride){
+						restoreInventory(p);
+						sender.sendMessage(p + "'s inventory has been restored.");
 					}
-					p.getInventory().setContents(items.toArray(new ItemStack[items.size()]));
-
-					List<ItemStack> armorItems = new ArrayList<ItemStack>();
-					for(int x = 0; x < 4; x++){
-						armorItems.add(getConfig().getItemStack(p.getName() + "a" + x));
+					else {
+						sender.sendMessage(restoreInventoryIfSupposedTo(p));
 					}
-					p.getInventory().setArmorContents(armorItems.toArray(new ItemStack[armorItems.size()]));
-					sender.sendMessage(p.getName()+"'s inventory has been restored up.");
 					return true;
 				} else {
 					sender.sendMessage(args[0] + " is offline or does not exist. Sorry");
@@ -99,33 +106,67 @@ public class SimpleInventoryBackup  extends JavaPlugin {
 		}
 		return false;
 	}
+	public String restoreInventoryIfSupposedTo(Player p){ //Checks if config permits player's inventory to be backed up
+		if (includeAllWorlds == true && !worldsToExclude.contains(p.getWorld())){
+			restoreInventory(p);
+			return p.getName() + "'s inventory has been restored.";
+		}
+		else if (worldsToInclude.contains(p.getWorld())){
+			restoreInventory(p);
+			return p.getName() + "'s inventory has been restored.";
+		}
+		else{
+			return p.getName() +"'s inventory has not been restored due to config settings.";
+		}
+	}
+	public void restoreInventory(Player p){
+		List<ItemStack> items = new ArrayList<ItemStack>();
+		for(int x = 0; x < 36; x++){
+			items.add(yaml.getItemStack(p.getName() + x));
+		}
+		p.getInventory().setContents(items.toArray(new ItemStack[items.size()]));
+
+		List<ItemStack> armorItems = new ArrayList<ItemStack>();
+		for(int x = 0; x < 4; x++){
+			armorItems.add(yaml.getItemStack(p.getName() + "a" + x));
+		}
+		p.getInventory().setArmorContents(armorItems.toArray(new ItemStack[armorItems.size()]));	
+	}
+	//
+	//INVENTORY BACKUP METHODS
+	//
 	public boolean backupInventory(CommandSender sender, Command cmd, String label, String[] args) {
 		if (args.length == 1){
 			if(args[0].equalsIgnoreCase("all")){
 				sender.sendMessage("Backing up all inventories");
 				for(Player p : Bukkit.getServer().getOnlinePlayers()) {
-					for (int x = 0; x < p.getInventory().getContents().length; x++){
-						getConfig().set(p.getName() + x, p.getInventory().getContents()[x]);
-					}
-					for (int x = 0; x < p.getInventory().getArmorContents().length; x++){
-						getConfig().set(p.getName() + "a" + x, p.getInventory().getArmorContents()[x]);
-					}
+					backupInventoryIfSupposedTo(p);
 				}
-				saveConfig();
+				try {
+					yaml.save(f);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				sender.sendMessage("Done backing up all inventories.");
 				return true;
 			}
 			else {
 				Player p = Bukkit.getPlayer(args[0]);
 				if (p != null) {
-					for (int x = 0; x < p.getInventory().getContents().length; x++){
-						getConfig().set(p.getName() + x, p.getInventory().getContents()[x]);
+					if(individualPlayerOverride){
+						backupInventory(p);
+						sender.sendMessage(p + "'s inventory has been backed up.");
 					}
-					for (int x = 0; x < p.getInventory().getArmorContents().length; x++){
-						getConfig().set(p.getName() + "a" + x, p.getInventory().getArmorContents()[x]);
+					else {
+						sender.sendMessage(backupInventoryIfSupposedTo(p));
 					}
-					saveConfig();
-					sender.sendMessage(p.getName() + "'s inventory is backed up.");
+					try {
+						yaml.save(f);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 					return true;
 				} else {
 					sender.sendMessage(args[0] + " is offline or does not exist. Sorry");
@@ -135,6 +176,56 @@ public class SimpleInventoryBackup  extends JavaPlugin {
 			}
 		}
 		return false;
+	}
+	public String backupInventoryIfSupposedTo(Player p){ //Checks if config permits player's inventory to be backed up
+		if (includeAllWorlds == true && !worldsToExclude.contains(p.getWorld())){
+			backupInventory(p);
+			return p.getName() + "'s inventory has been backed up.";
+		}
+		else if (worldsToInclude.contains(p.getWorld())){
+			backupInventory(p);
+			return p.getName() + "'s inventory has been backed up.";
+		}
+		else{
+
+			return p.getName() +"'s inventory has not been backed up due to config settings.";
+		}
+	}
+	public void backupInventory(Player p){ //backs up player's inventory regardless of config
+		for (int x = 0; x < p.getInventory().getContents().length; x++){
+			yaml.set(p.getName() + x, p.getInventory().getContents()[x]);
+		}
+		for (int x = 0; x < p.getInventory().getArmorContents().length; x++){
+			yaml.set(p.getName() + "a" + x, p.getInventory().getArmorContents()[x]);
+		}
+	}
+	
+	//
+	//MISC METHODS
+	//
+	public void readConfig(){
+		individualPlayerOverride = getConfig().getBoolean("individualplayeroverride");
+		String worlds = getConfig().getString("includedworlds");
+		if(worlds == null){
+			noWorlds = true;
+		}
+		else if (worlds.equalsIgnoreCase("all")){
+			includeAllWorlds = true;
+		}
+		else {
+			for (String w: worlds.split(",")){
+				worldsToInclude.add(Bukkit.getWorld(w));
+			}
+		}
+		
+		
+		worlds = getConfig().getString("excludedworlds");
+		if(worlds != null){
+			for (String w: worlds.split(",")){
+				worldsToExclude.add(Bukkit.getWorld(w.trim()));
+			}
+		}
+		
 	}
 }
 class TheListener implements Listener {
